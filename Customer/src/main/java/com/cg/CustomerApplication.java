849f36fa01f53ccce21c1569f1c75dd49f651c60
shreyareddyg <gade.shreyareddy@gmail.com>
2020-05-09 12:59:08 +0530

    properties added

--- Customer/src/main/java/com/cg/CustomerApplication.java
+++ Customer/src/main/java/com/cg/CustomerApplication.java
@@ -2,10 +2,14 @@ package com.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableSwagger2
 public class CustomerApplication {
 
 	public static void main(String[] args) {
